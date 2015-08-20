29f40dbf6e3d96647de28617fd210caecb433503
Andrew Breckenridge <asbreckenridge@me.com>
2015-08-20 11:39:44 -0700

    init

---
+++ Slick/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Slick
+//
+//  Created by Andrew Breckenridge on 8/20/15.
+//  Copyright © 2015 Andrew Breckenridge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
